ca425d412d0daa90d7dd0f983845c186c3958ed7
Mino11206 <phamm0758@gmail.com>
2025-10-15 21:41:50 +0700

    First commit

---
+++ AI.py
@@ -0,0 +1,19 @@
+import google.generativeai as gemini
+import os
+import streamlit as st
+
+st.title("ChatBot du lịch thông minh:")
+
+os.environ["GRPC_VERBOSITY"] = "NONE"
+os.environ["GLOG_minloglevel"] = "2"
+
+gemini.configure(api_key="AIzaSyAYRPfEJBhxVhAVE8Xqbwd2sebPsOPSI_w")
+model = gemini.GenerativeModel("gemini-2.5-flash")
+
+query = st.text_input("Nhập vào câu hỏi của bạn: ", key = "query_input")
+if st.button("Gửi"):
+    response = model.generate_content(query)
+    st.success(response.text)
+
+
+        
---
+++ demo.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    print(str(1+i))
+cd OneDrive/Desktop/myChatBot
+py -m streamlit run AI.py
