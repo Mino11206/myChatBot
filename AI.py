e2df0149692d0a5acbbabde61c7180e50ccdc58a
Mino11206 <phamm0758@gmail.com>
2025-10-19 23:30:04 +0700

    Thêm bộ nhớ

--- AI.py
+++ AI.py
@@ -10,9 +10,13 @@ os.environ["GLOG_minloglevel"] = "2"
 gemini.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = gemini.GenerativeModel("gemini-2.5-flash")
 
-query = st.text_input("Nhập vào câu hỏi của bạn: ", key = "query_input")
+history =[""]
+context = "Bạn là TravelBot một tư vấn viên du lịch chuyên nghiệp, hãy dùng tiếng việt một cách chuyên nghiệp và cuốn hút"
+query = "context: " + context + ", your previous response: " + history[-1]
+query += st.text_input("Nhập vào câu hỏi của bạn: ", key = "query_input")
 if st.button("Gửi"):
     response = model.generate_content(query)
+    history.append(response.text)
     st.success(response.text)
 
 
